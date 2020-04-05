da262cbf2a0c192ba48a0d201982998b890b3fa1
Dominic DeJesus <domludejesus@gmail.com>
2020-04-05 12:42:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-def #introduction(name, language)
+def #introduction_with_language(name, language)
   
-  puts 'Hi my name is #{name} and I program with #{language}'
+  puts "Hi, my name is #{name} and am learning to program in #{language}""
   
   
 end 
 
-#introduction('Dom', 'Ruby')
+#introduction_with_language("Dom", "Ruby")
